dead69db197770023273c43d470619cb9e193b10
Gustavo Bigardi <guhbigardi@hotmail.com>
2018-04-10 00:13:46 -0300

    criação do projeto

---
+++ src/ActiveLearningLambdasBot/Dialogs/MainPage.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace ActiveLearningBot.Dialogs
+{
+    [Serializable]
+    public class MainDialog : BaseLuisDialog<object>
+    {
+        [NonSerialized]
+        private LuisLearnService luisLearnService;
+
+        public MainDialog()
+        {
+          
+        }
+
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            context.UserData.SetValue("MessageId", result.Query);
+            var card = new HeroCard
+            {
+                Title = "Desculpe...",
+                Text = "Ainda estou aprendendo, qual dessas opções representa o que você deseja?",
+                Buttons = GetCardActions()
+            };
+
+            var activity = context.MakeMessage();
+            activity.Id = new Random().Next().ToString();
+            activity.Attachments.Add(card.ToAttachment());
+
+            await context.PostAsync(activity);
+        }
+
+        private static List<CardAction> GetCardActions()
+        {
+            return new List<CardAction>
+            {
+                new CardAction
+                {
+                    Title = "Desejo apontar as horas",
+                    Type = ActionTypes.ImBack,
+                    Value = "Desejo apontar as horas"
+                },
+                new CardAction
+                {
+                    Title = "Calma Fera, Só estou te cumprimentando",
+                    Type = ActionTypes.ImBack,
+                    Value = "Só estou te cumprimentando"
+                },
+                new CardAction
+                {
+                    Title = "Nenhuma",
+                    Type = ActionTypes.ImBack,
+                    Value = "Nenhuma"
+                }
+            };
+        }
+
+        [LuisIntent("Saudacao")]
+        public async Task Saudacao(IDialogContext context, LuisResult result)
+        {
+            LearnLatestMessageSended(result.TopScoringIntent.Intent, context);
+
+            await context.PostAsync($"Bem-vindo(a) a Lambda3! Sou o seu assistente virtual, ja apontou suas horas hoje? 🕵");
+            context.Wait(MessageReceived);
+        }
+
+
+        [LuisIntent("Requisitar.Ajuda.Bot")]
+        public async Task ApontamentoHora(IDialogContext context, LuisResult result)
+        {
+            LearnLatestMessageSended(result.TopScoringIntent.Intent, context);
+
+            await context.PostAsync($"https://spine.lambda3.com.br/");
+            context.Wait(MessageReceived);
+        }
+
+        private void LearnLatestMessageSended(string intent, IDialogContext dialogContext)
+        {
+            luisLearnService = new LuisLearnService();
+            luisLearnService.SetNameApp().GetAwaiter();
+
+            HostingEnvironment.QueueBackgroundWorkItem(workItem =>
+                luisLearnService?.LearnLatestMessageSended(intent, dialogContext));
+        }
+
+    }
+}
---
+++ src/ActiveLearningLambdasBot/Services/LuisLearnService.cs
@@ -0,0 +1,65 @@
+﻿using Cognitive.LUIS.Programmatic;
+using Cognitive.LUIS.Programmatic.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActiveLearningBot
+{
+    [Serializable]
+    public class LuisLearnService
+    {
+        private LuisApp luisApp;
+        private LuisProgClient luisProgClient;
+
+        public LuisLearnService()
+        {
+            var subscriptionKey = ConfigurationManager.AppSettings.Get("LuisSubscriptionKey");
+            luisProgClient = new LuisProgClient(subscriptionKey, Location.WestUS);
+        }
+
+        public async Task SetNameApp() => luisApp = await luisProgClient.GetAppByNameAsync(ConfigurationManager.AppSettings.Get("LuisAppName"));
+
+        public async Task LearnLatestMessageSended(string intent, IDialogContext dialogContext)
+        {
+            var message = dialogContext.UserData.GetValue<string>("MessageId");
+            if (string.IsNullOrEmpty(message)) return;
+
+            await Learn(message, intent);
+            dialogContext.UserData.RemoveValue("MessageId");
+        }
+
+        public async Task Learn(string message, string intent)
+        {
+            var example = CreateExample(message, intent);
+
+            var app = await luisProgClient.GetAppByNameAsync("PresentationBot");
+            await luisProgClient.AddExampleAsync(app.Id, app.Endpoints.Production.VersionId, example);
+
+            await luisProgClient.TrainAsync(app.Id, app.Endpoints.Production.VersionId);
+            await VerifyStatusTraining(luisProgClient, app);
+
+            await luisProgClient.PublishAsync(app.Id, app.Endpoints.Production.VersionId, false, "westus");
+        }
+
+        private async Task VerifyStatusTraining(LuisProgClient client, LuisApp app)
+        {
+            IEnumerable<Training> trainingList;
+            do
+            {
+                trainingList = await client.GetTrainingStatusListAsync(app.Id, app.Endpoints.Production.VersionId);
+            }
+            while (!trainingList.All(x => x.Details.Status.Equals("Success")));
+        }
+
+        private Example CreateExample(string message, string intent) => new Example
+        {
+            Text = message,
+            IntentName = intent
+        };
+
+    }
+}
